9d0d60b137eb744ae1cf51c3d180a38199e6cbc6
Adderly Jauregui <adderly.jauregui@globant.com>
2018-01-02 01:19:22 -0600

    Working in the visibility of the transactions and accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :accounts, foreign_key: :user_id
+  has_many :transactions, foreign_key: :user_id
 
   #validations
   validates_presence_of :name, :email, :password_digest
