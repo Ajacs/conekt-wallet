85f63d660f60ea304778eda885159efd58cf3546
Adderly Jauregui <adderly.jauregui@globant.com>
2018-01-08 12:03:24 -0600

    Updated the documentation about the use of docker

--- spec/services/transactions_service_spec.rb
+++ spec/services/transactions_service_spec.rb
@@ -1,8 +1,4 @@
 RSpec.describe TransactionsService do
   describe 'Initialize the service' do
-
-    it 'create a new instance of the transference service' do
-      expect().to ca
-    end
   end
 end
